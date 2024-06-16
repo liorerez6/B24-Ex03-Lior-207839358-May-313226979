398ae57cb079fd9d0d4d9ec7c3eca40de4c81456
Lior <liorerez6@gmail.com>
2024-06-16 12:26:10 +0300

    גכדש

--- Ex03.GarageLogic/Truck.cs
+++ Ex03.GarageLogic/Truck.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using static Ex03.GameLogic.Enums;
 
 namespace Ex03.GameLogic
 {
     internal class Truck : Vehicle
     {
         private bool r_IsCarringToxicSubstances;
-        private float m_EngineCapacity;
-        private List<Wheel> m_Wheels;
+        private float m_CargoCapacity;
+        private List<Wheel> m_Wheels = new List<Wheel>();
+        const ETypeOfFuel k_TypeOfFuel = ETypeOfFuel.Soler;
 
+        private const int k_MaxTiresPressure = 28;
+        private const float k_MaxFuelTank = 120F;
+        private float m_CurrentFuelAmount;
+        
         //METHODS:
 
     }
